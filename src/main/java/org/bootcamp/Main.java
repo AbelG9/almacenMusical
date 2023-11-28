0b632415e36401ce2744f44583561b8a3b542304
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-11-27 21:37:51 -0500

    implemented database connection

--- src/main/java/org/bootcamp/Main.java
+++ src/main/java/org/bootcamp/Main.java
@@ -1,7 +1,14 @@
 package org.bootcamp;
 
+import org.bootcamp.utils.DbConnection;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DbConnection dbConnection = new DbConnection();
+        Connection connection = dbConnection.getConnection();
+
+
     }
 }
